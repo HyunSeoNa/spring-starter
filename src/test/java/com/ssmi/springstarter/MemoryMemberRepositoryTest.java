c2669ebaed3fc3acc3395b39d7f40b423e72b4fe
hyunseo <nahs3467@gmail.com>
2025-10-13 10:41:15 +0900

    create member repositoy test case

---
+++ src/test/java/com/ssmi/springstarter/MemoryMemberRepositoryTest.java
@@ -0,0 +1,68 @@
+package com.ssmi.springstarter;
+
+import com.ssmi.springstarter.domain.Member;
+import com.ssmi.springstarter.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * packageName    : com.ssmi.springstarter
+ * fileName       : MemoryMemberRepositoryTest
+ * author         : ssmihs
+ * date           : 2025-10-13
+ * description    :
+ */
+public class MemoryMemberRepositoryTest {
+
+    MemoryMemberRepository repository = new MemoryMemberRepository();
+
+    @AfterEach
+    public void afterEach() {
+        repository.clearStore();
+    }
+
+    @Test
+    public void save() {
+        Member member = new Member();
+        member.setName("spring");
+
+        repository.save(member);
+
+        Member result = repository.findById(member.getId()).get();
+        assertThat(result).isEqualTo(member);
+    }
+
+    @Test
+    public void findByName() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        Member result = repository.findByName("spring1").get();
+
+        assertThat(result).isEqualTo(member1);
+    }
+
+    @Test
+    public void findAll() {
+        Member member1 = new Member();
+        member1.setName("spring1");
+        repository.save(member1);
+
+        Member member2 = new Member();
+        member2.setName("spring2");
+        repository.save(member2);
+
+        List<Member> result = repository.findAll();
+
+        assertThat(result.size()).isEqualTo(2);
+    }
+}
