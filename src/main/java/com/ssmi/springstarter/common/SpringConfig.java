2f90ad3e75b4fc424aec75936fa463f81a7ddfb9
hyunseo <hforestna@gmail.com>
2025-10-13 21:38:17 +0900

    create jdbc member repository

--- src/main/java/com/ssmi/springstarter/common/SpringConfig.java
+++ src/main/java/com/ssmi/springstarter/common/SpringConfig.java
@@ -1,10 +1,13 @@
 package com.ssmi.springstarter.common;
 
+import com.ssmi.springstarter.repository.JdbcMemberRepository;
 import com.ssmi.springstarter.repository.MemberRepository;
 import com.ssmi.springstarter.repository.MemoryMemberRepository;
 import com.ssmi.springstarter.service.MemberService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
 
 /**
  * packageName    : com.ssmi.springstarter.common
@@ -16,6 +19,12 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class SpringConfig {
 
+    private final DataSource dataSource;
+
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -23,6 +32,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+//        return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
---
+++ src/main/java/com/ssmi/springstarter/repository/JdbcMemberRepository.java
@@ -0,0 +1,101 @@
+package com.ssmi.springstarter.repository;
+
+import com.ssmi.springstarter.domain.Member;
+import org.springframework.jdbc.datasource.DataSourceUtils;
+
+import javax.sql.DataSource;
+import java.sql.*;
+import java.util.List;
+import java.util.Optional;
+
+/**
+ * fileName     : null.java
+ * author       : hyunseo
+ * date         : 2025. 10. 13.
+ * description  :
+ */
+public class JdbcMemberRepository implements MemberRepository {
+
+    private final DataSource dataSource;
+
+    public JdbcMemberRepository(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public Member save(Member member) {
+        String sql = "select * from member where id = ?";
+
+        Connection conn = null;
+        PreparedStatement pstmt = null;
+        ResultSet rs = null;
+
+        try {
+            conn = getConnection();
+            pstmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+
+            pstmt.setString(1, member.getName());
+
+            pstmt.executeUpdate();
+            rs = pstmt.getGeneratedKeys();
+
+            if(rs.next()) {
+                member.setId(rs.getLong(1));
+            } else {
+                throw new SQLException("id 조회 실패");
+            }
+
+            return member;
+        } catch (Exception e) {
+            throw new IllegalStateException(e);
+        } finally {
+            close(conn, pstmt, rs);
+        }
+
+    }
+
+    @Override
+    public Optional<Member> findById(Long id) {
+        return Optional.empty();
+    }
+
+    @Override
+    public Optional<Member> findByName(String name) {
+        return Optional.empty();
+    }
+
+    @Override
+    public List<Member> findAll() {
+        return List.of();
+    }
+
+    private Connection getConnection() {
+        return DataSourceUtils.getConnection(dataSource);
+    }
+
+    private void close(Connection conn, PreparedStatement pstmt, ResultSet rs) {
+        try {
+            if(rs != null) {
+                rs.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            if(pstmt != null) {
+                pstmt.close();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        if(conn != null) {
+            close(conn);
+        }
+    }
+
+    private void close(Connection conn) {
+        DataSourceUtils.releaseConnection(conn, dataSource);
+    }
+}
